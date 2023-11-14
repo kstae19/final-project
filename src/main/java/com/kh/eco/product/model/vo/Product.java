51f56f88d18c008250164a5876404469e17f2b84
CamoPepper <leny0215@gmail.com>
2023-11-14 12:56:21 +0900

    hs

---
+++ src/main/java/com/kh/eco/product/model/vo/Product.java
@@ -0,0 +1,12 @@
+package com.kh.eco.product.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor @Setter @Getter @ToString
+public class Product {
+	private int productNo;
+}
