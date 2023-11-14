63c4db38c3d05c3d6c63398d4f4c258da782cefc
kstae19 <kstae19@naver.com>
2023-11-14 14:19:17 +0900

    Create User.java

---
+++ src/main/java/com/kh/eco/user/model/vo/User.java
@@ -0,0 +1,21 @@
+package com.kh.eco.user.model.vo;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor @Getter @Setter @ToString
+public class User {
+	private int userNo; // USER_NO	NUMBER
+	private String userId; // USER_ID	VARCHAR2(50 BYTE)
+	private String userPwd; // USER_PWD	VARCHAR2(50 BYTE)
+	private String userName; // USER_NAME	VARCHAR2(50 BYTE)
+	private String email; // EMAIL	VARCHAR2(50 BYTE)
+	private String phone; // PHONE	VARCHAR2(13 BYTE)
+	private int point; // POINT	NUMBER
+	private String userStatus; // USER_STATUS	CHAR(1 BYTE)
+	private Date enrollDate; // ENROLL_DATE	DATE
+}
