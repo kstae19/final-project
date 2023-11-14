072b8dd9e21377ce8dcf915e6d245308951dc6f4
kstae19 <kstae19@naver.com>
2023-11-14 18:28:37 +0900

    Update User.java

--- src/main/java/com/kh/eco/user/model/vo/User.java
+++ src/main/java/com/kh/eco/user/model/vo/User.java
@@ -16,6 +16,9 @@ public class User {
 	private String email; // EMAIL	VARCHAR2(50 BYTE)
 	private String phone; // PHONE	VARCHAR2(13 BYTE)
 	private int point; // POINT	NUMBER
+	private int post; // POST NUMBER NULL,
+	private String address; // ADDRESS VARCHAR2(200) NULL,
+	private String detailAddress; // DETAIL_ADDRESS VARCHAR2(50) NULL,
 	private String userStatus; // USER_STATUS	CHAR(1 BYTE)
 	private Date enrollDate; // ENROLL_DATE	DATE
 }
