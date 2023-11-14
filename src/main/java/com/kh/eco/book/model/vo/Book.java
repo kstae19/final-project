6466fe155b15a7e97917dc9f29277dc961eee2c2
aadamk613 <aadamk@naver.com>
2023-11-14 12:45:15 +0900

    ㅋㅋ;

--- src/main/java/com/kh/eco/book/model/vo/Book.java
+++ src/main/java/com/kh/eco/book/model/vo/Book.java
@@ -7,5 +7,13 @@ public class Book {
 	
 	private String ISBN13;
 	private int bookCount;
+	private String bookTitle;
+	private String bookWriter;
+	private String bookImg;
+	private String bookPublisher;
+	private String bookContent;
+	private String bookDate;
+	private String bookCategory;
+	private String bookLink;
 	
 }
