b71f16eb4e77eb063aeca33301ded79f104b44be
kstae19 <kstae19@naver.com>
2023-11-15 17:09:22 +0900

    Update UserDao.java

--- src/main/java/com/kh/eco/user/model/dao/UserDao.java
+++ src/main/java/com/kh/eco/user/model/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.kh.eco.user.model.dao;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.eco.user.model.vo.Cert;
 import com.kh.eco.user.model.vo.User;
 
 @Repository
@@ -12,6 +13,23 @@ public class UserDao {
 		User user = sqlSession.selectOne("userMapper.loginUser", u);
 		System.out.println(user);
 		return sqlSession.selectOne("userMapper.loginUser", u);
+	}
+
+	public int idCheck(SqlSessionTemplate sqlSession, String checkId) {
+		return sqlSession.selectOne("userMapper.idCheck", checkId);
+	}
+
+	public int insertSecret(SqlSessionTemplate sqlSession, Cert cert) {
+		return sqlSession.insert("userMapper.insertSecret", cert);
+	}
+
+	public boolean vaildate(SqlSessionTemplate sqlSession, Cert cert) {
+		Cert result = sqlSession.selectOne("userMapper.vaildate", cert);
+		return result != null;
+	}
+
+	public void deleteCert(SqlSessionTemplate sqlSession, Cert cert) {
+		sqlSession.delete("userMapper.deleteCert", cert);
 	}
 
 }
