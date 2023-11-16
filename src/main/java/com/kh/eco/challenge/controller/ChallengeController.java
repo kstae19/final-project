0dfe2a753170e4a523b6fc8ccc687e8113f7133c
heeay <144909009+heeay@users.noreply.github.com>
2023-11-16 14:41:32 +0900

    listView 데이터 뿌리기

--- src/main/java/com/kh/eco/challenge/controller/ChallengeController.java
+++ src/main/java/com/kh/eco/challenge/controller/ChallengeController.java
@@ -6,9 +6,13 @@ import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.eco.challenge.model.service.ChallengeService;
+import com.kh.eco.common.model.template.Pagination;
+import com.kh.eco.common.model.vo.PageInfo;
 
 @Controller
 public class ChallengeController {
@@ -17,12 +21,32 @@ public class ChallengeController {
 	private ChallengeService challengeService;
 	
 	@RequestMapping("challenge")
-	public String challenge() throws IOException {
-
+	public String selectChallengeList(@RequestParam(value="currentPage", defaultValue="1")int currentPage, Model model) throws IOException {
+		
+		/*내 답안 ==> private이라서 setter,getter로 바로 못 받음
+		 * PageInfo pi = new PageInfo();
+		 * pi.setListCount(challengeService.countChallengeList())
+		 */
+		
+		PageInfo pi = Pagination.getPageInfo( // 1. listCount부터 알아오기
+											challengeService.countChallengeList(),
+											currentPage,
+											4,
+											5
+											);
+		// System.out.println(challengeService.countChallengeList());
+		
+		// 2. 페이징정보 불러오기 : model, modelAndView, session 셋 중 하나
+		model.addAttribute("list", challengeService.selectChallengeList(pi));
+		//model.addAttribute("pi", pi);
+		System.out.println(challengeService.selectChallengeList(pi));
+		
+		// 3. 화면 포워딩하기   WEB-INF/views/        "요기"           .jsp
 		return "challenge/challengeListView";
 		
 	}
 	
+
 	@RequestMapping("increase.like")
 	public void increaseLikeCount(int userNo, int challengeNo) {
 		
--- src/main/java/com/kh/eco/challenge/model/dao/ChallengeDao.java
+++ src/main/java/com/kh/eco/challenge/model/dao/ChallengeDao.java
@@ -13,58 +13,58 @@ import com.kh.eco.challenge.model.vo.Challenge;
 public class ChallengeDao {
 
 	public int countChallengeList(SqlSessionTemplate sqlSession) {
-		// TODO Auto-generated method stub
+		
 		return sqlSession.selectOne("challengeMapper.countChallengeList");
 	}
 
 	public ArrayList<Challenge> selectChallengeList(SqlSessionTemplate sqlSession, RowBounds rowBounds) {
-		// TODO Auto-generated method stub
-		return (ArrayList)sqlSession.selectList("challengeMapper.selectChallengeList", rowBounds);
+		
+		return (ArrayList)sqlSession.selectList("challengeMapper.selectChallengeList", null, rowBounds);
 	}
 
 	public int countChallengeSearch(SqlSessionTemplate sqlSession, HashMap<String, String> map) {
-		// TODO Auto-generated method stub
+		
 		return sqlSession.selectOne("challengeMapper.countChallengeSearch", map);
 	}
 
 	public ArrayList<Challenge> selectSearchList(SqlSessionTemplate sqlSession, HashMap<String, String> map,
 			RowBounds rowBounds) {
-		// TODO Auto-generated method stub
+		
 		return (ArrayList)sqlSession.selectList("challengeMapper.selectSearchList", map, rowBounds);
 	}
 
 	public int increaseViewCount(SqlSessionTemplate sqlSession, int challengeNo) {
-		// TODO Auto-generated method stub
+		
 		return sqlSession.update("challengeMapper.increaseViewCount", challengeNo);
 	}
 
 	public Challenge selectChallengeDetail(SqlSessionTemplate sqlSession, int challengeNo) {
-		// TODO Auto-generated method stub
+		
 		return sqlSession.selectOne("challengeMapper.selectChallengeDetail", challengeNo);
 	}
 	
 	public int increaseLikeCount(SqlSessionTemplate sqlSession, HashMap<String, Integer> map) {
-		// TODO Auto-generated method stub
+		
 		return sqlSession.update("challengeMapper.increaseLikeCount", map);
 	}
 
 	public int decreaseLikeCount(SqlSessionTemplate sqlSession, HashMap<String, Integer> map) {
-		// TODO Auto-generated method stub
+		
 		return sqlSession.update("challengeMapper.decreaseLikeCount", map);
 	}
 
 	public int insertChallenge(SqlSessionTemplate sqlSession, Challenge c) {
-		// TODO Auto-generated method stub
+		
 		return sqlSession.insert("challengeMapper.insertChallenge", c);
 	}
 
 	public int updateChallenge(SqlSessionTemplate sqlSession, Challenge c) {
-		// TODO Auto-generated method stub
+		
 		return sqlSession.update("challengeMapper.updateChallenge", c);
 	}
 
 	public int deleteChallenge(SqlSessionTemplate sqlSession, int challengeNo) {
-		// TODO Auto-generated method stub
+		
 		return sqlSession.update("challengeMapper.deleteChallenge", challengeNo);
 	}
 
