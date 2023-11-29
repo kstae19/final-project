e5433f2557f1a5a4be66a4ed0a25040fa039d72e
heeay <144909009+heeay@users.noreply.github.com>
2023-11-29 12:34:59 +0900

    이벤트인터페이스만 다시 수정

--- src/main/java/com/kh/eco/event/model/service/EventService.java
+++ src/main/java/com/kh/eco/event/model/service/EventService.java
@@ -9,24 +9,9 @@ import com.kh.eco.event.model.vo.Event;
 
 public interface EventService {
 	
-	// 게시글 작성
-	public void create(EventBoard eBoard) throws Exception;
-	// 게시글 상세보기
-	public EventBoard read(int bno) throws Exception;
 	
-	
-	
-	
-	
-
-	// 전체 이벤트 조회
 	ArrayList<Event> selectEventList();
-	
-	// 세부 이벤트 조회
 	Event selectEventDetail(int eventNo);
-	
-	
-	// 이벤트 등록, 수정, 삭제
 	int insertEvent(Event event);
 	int updateEvent(Event event);
 	int deleteEvent(int eventNo);
