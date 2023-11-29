d1cbdd1c1032b62c0438a2f6c2b83554b8cf011b
kstae19 <kstae19@naver.com>
2023-11-29 14:51:40 +0900

    Update PublicTransportController.java

--- src/main/java/com/kh/eco/move/controller/PublicTransportController.java
+++ src/main/java/com/kh/eco/move/controller/PublicTransportController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PublicTransportController {
 	
 	static final String SB_SERVICEKEY = "abfa69d1-b435-47be-8386-075f6227a0d8";
+	static final String SI_SERVICEKEY = "4148426f426b737437307945627249";
 	
 	@ResponseBody
 	@RequestMapping(value="subwayST", produces="application/json; charset=UTF-8")
@@ -36,5 +37,28 @@ public class PublicTransportController {
 		urlConnection.disconnect();
 		
 		return subwayStText;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="subway", produces="application/json; charset=UTF-8")
+	public String subwayInfo(String sbName) throws IOException {
+		String url = "http://swopenapi.seoul.go.kr/api/subway/";
+		url += URLEncoder.encode(SI_SERVICEKEY, "UTF-8");
+		url += "/json/realtimeStationArrival/0/50/";
+		url += URLEncoder.encode(sbName, "UTF-8");
+		
+		System.out.println(url);
+		URL requestUrl = new URL(url);
+		HttpURLConnection urlConnection = (HttpURLConnection)requestUrl.openConnection();
+		urlConnection.setRequestMethod("GET");
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		String subwaySiText = br.readLine();
+		
+		System.out.println(subwaySiText);
+		
+		br.close();
+		urlConnection.disconnect();
+		
+		return subwaySiText;
 	}
 }
