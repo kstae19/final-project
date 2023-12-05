6cc6ae3eac1db3511562ac240bb46d6ff75a856e
kstae19 <kstae19@naver.com>
2023-12-05 17:06:36 +0900

    Update PublicTransportController.java

--- src/main/java/com/kh/eco/move/controller/PublicTransportController.java
+++ src/main/java/com/kh/eco/move/controller/PublicTransportController.java
@@ -7,6 +7,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,15 +16,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PublicTransportController {
 	
-	static final String SB_SERVICEKEY = "abfa69d1-b435-47be-8386-075f6227a0d8";
-	static final String SI_SERVICEKEY = "4148426f426b737437307945627249";
-	static final String SN_SERVICEKEY = "437062554a6b7374313038665942434d";
+	@Value("${key.SB}")
+	private String SBApiKey;
+	
+	@Value("${key.SI}")
+	private String SIApiKey;
+	
+	@Value("${key.SN}")
+	private String SNApiKey;
 	
 	@ResponseBody
 	@RequestMapping(value="subwayST", produces="application/json; charset=UTF-8")
 	public String subwayStation() throws IOException {
 		String url = "https://t-data.seoul.go.kr/apig/apiman-gateway/tapi/TaimsKsccDvSubwayStationGeom/1.0";
-		url += "?apikey=" + URLEncoder.encode(SB_SERVICEKEY, "UTF-8");
+		url += "?apikey=" + URLEncoder.encode(SBApiKey, "UTF-8");
 		
 		// System.out.println(url);
 		URL requestUrl = new URL(url);
@@ -46,7 +52,7 @@ public class PublicTransportController {
 		String subC = "";
 		
 		String url = "http://openapi.seoul.go.kr:8088/";
-		url += URLEncoder.encode(SN_SERVICEKEY, "UTF-8");
+		url += URLEncoder.encode(SNApiKey, "UTF-8");
 		url += "/json/SearchSTNBySubwayLineInfo/1/1/%20/";
 		url += subName + "/";
 		
@@ -156,7 +162,7 @@ public class PublicTransportController {
 	@RequestMapping(value="subtime", produces="application/json; charset=UTF-8")
 	public String subtimeInfo(String subcode) throws IOException {
 		String url = "http://openapi.seoul.go.kr:8088/";
-		url += URLEncoder.encode(SN_SERVICEKEY, "UTF-8");
+		url += URLEncoder.encode(SNApiKey, "UTF-8");
 		url += "/json/SearchSTNTimeTableByIDService/1/500/";
 		url += subcode + "/1/1";
 		// System.out.println(url);
@@ -178,7 +184,7 @@ public class PublicTransportController {
 	@RequestMapping(value="subtime2", produces="application/json; charset=UTF-8")
 	public String subtime2Info(String subcode) throws IOException {
 		String url = "http://openapi.seoul.go.kr:8088/";
-		url += URLEncoder.encode(SN_SERVICEKEY, "UTF-8");
+		url += URLEncoder.encode(SNApiKey, "UTF-8");
 		url += "/json/SearchSTNTimeTableByIDService/1/500/";
 		url += subcode + "/1/2";
 		// System.out.println(url);
@@ -200,7 +206,7 @@ public class PublicTransportController {
 	@RequestMapping(value="subway", produces="application/json; charset=UTF-8")
 	public String subwayInfo(String sName) throws IOException {
 		String url = "http://swopenapi.seoul.go.kr/api/subway/";
-		url += "sample"; //URLEncoder.encode(SI_SERVICEKEY, "UTF-8");
+		url += "sample"; //URLEncoder.encode(SIApiKey, "UTF-8");
 		url += "/json/realtimeStationArrival/0/5/";
 		url += URLEncoder.encode(sName, "UTF-8");
 		
